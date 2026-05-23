2888d8654c64282969085c0baca92d7e9d54677c
Jonathan150408 <jonathan.junod@eduvaud.ch>
2026-05-23 14:02:49 +0200

    fix(navigation) : retirer les doublons

--- FlashQuizz-v2/AppShell.xaml.cs
+++ FlashQuizz-v2/AppShell.xaml.cs
@@ -7,13 +7,6 @@ namespace FlashQuizz_v2
         public AppShell()
         {
             InitializeComponent();
-
-            var flyout = new FlyoutItem { FlyoutDisplayOptions = FlyoutDisplayOptions.AsMultipleItems };
-            flyout.Items.Add(new ShellContent { Title = "Decks", Content = new DecksPage() });
-            flyout.Items.Add(new ShellContent { Title = "Profil", Content = new ProfilePage() });
-            flyout.Items.Add(new ShellContent { Title = "Infos", Content = new InfosPage() });
-
-            Items.Add(flyout);
         }
     }
 }
